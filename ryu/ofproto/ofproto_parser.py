1866e2dd876dac4a0a8d30e2f46eb5db369ff1eb
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2012-01-25 15:53:54 -0800

    move MsgBase class to ofproto_parser.py from ofproto_parser_v1_0.py
    This moves MsgBase class to ofproto_parser.py from
    ofproto_parser_v1_0.py in order to enable OF 1.2 version parser to use
    MsgBase class.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/ofproto/ofproto_parser.py
+++ ryu/ofproto/ofproto_parser.py
@@ -47,3 +47,69 @@ def msg(datapath, version, msg_type, msg_len, xid, buf):
         raise exception.OFPUnknownVersion(version=version)
 
     return msg_parser(datapath, version, msg_type, msg_len, xid, buf)
+
+
+class MsgBase(object):
+    def __init__(self, datapath):
+        self.datapath = datapath
+        self.version = None
+        self.msg_type = None
+        self.msg_len = None
+        self.xid = None
+        self.buf = None
+
+    def set_headers(self, version, msg_type, msg_len, xid):
+        assert msg_type == self.cls_msg_type
+
+        self.version = version
+        self.msg_type = msg_type
+        self.msg_len = msg_len
+        self.xid = xid
+
+    def set_buf(self, buf):
+        self.buf = buffer(buf)
+
+    def __str__(self):
+        return 'version: 0x%x msg_type 0x%x xid 0x%x' % (self.version,
+                                                         self.msg_type,
+                                                         self.xid)
+
+    @classmethod
+    def parser(cls, datapath, version, msg_type, msg_len, xid, buf):
+        msg = cls(datapath)
+        msg.set_headers(version, msg_type, msg_len, xid)
+        msg.set_buf(buf)
+        return msg
+
+    def _serialize_pre(self):
+        assert self.version is None
+        assert self.msg_type is None
+        assert self.buf is None
+
+        self.version = self.datapath.ofproto.OFP_VERSION
+        self.msg_type = self.cls_msg_type
+        self.buf = bytearray().zfill(self.datapath.ofproto.OFP_HEADER_SIZE)
+
+    def _serialize_header(self):
+        # buffer length is determined after trailing data is formated.
+        assert self.version is not None
+        assert self.msg_type is not None
+        assert self.msg_len is None
+        assert self.xid is None
+        assert self.buf is not None
+        assert len(self.buf) >= self.datapath.ofproto.OFP_HEADER_SIZE
+
+        self.msg_len = len(self.buf)
+        self.xid = 0  # TODO:XXX
+
+        struct.pack_into(self.datapath.ofproto.OFP_HEADER_PACK_STR, self.buf, 0,
+                         self.version, self.msg_type, self.msg_len, self.xid)
+
+    def _serialize_body(self):
+        pass
+
+    def serialize(self):
+        self._serialize_pre()
+        self._serialize_body()
+        self._serialize_header()
+
