9314fa4c64a2b8b21d3f749c12cce348dfc1966c
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2012-10-04 22:21:20 +0900

    packet lib: add mpls
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/lib/packet/mpls.py
+++ ryu/lib/packet/mpls.py
@@ -14,34 +14,38 @@
 # limitations under the License.
 
 import struct
+import socket
 from . import packet_base
-from . import vlan
-from . import mpls
+from . import packet_utils
+from . import ipv4
 from ryu.ofproto import ether
 
 
-class ethernet(packet_base.PacketBase):
-    _PACK_STR = '!6s6sH'
+class mpls(packet_base.PacketBase):
+    _PACK_STR = '!I'
     _MIN_LEN = struct.calcsize(_PACK_STR)
 
-    def __init__(self, dst, src, ethertype):
-        super(ethernet, self).__init__()
-        self.dst = dst
-        self.src = src
-        self.ethertype = ethertype
-        self.length = ethernet._MIN_LEN
+    def __init__(self, label, exp, bsb, ttl):
+        super(mpls, self).__init__()
+        self.label = label
+        self.exp = exp
+        self.bsb = bsb
+        self.ttl = ttl
+        self.length = mpls._MIN_LEN
 
     @classmethod
     def parser(cls, buf):
-        dst, src, ethertype = struct.unpack_from(cls._PACK_STR, buf)
-        return cls(dst, src, ethertype), ethernet.get_packet_type(ethertype)
+        (label,) = struct.unpack_from(cls._PACK_STR, buf)
+        ttl = label & 0xff
+        bsb = (label >> 8) & 1
+        exp = (label >> 9) & 7
+        label = label >> 12
+        msg = cls(label, exp, bsb, ttl)
+        if bsb:
+            return msg, ipv4.ipv4
+        else:
+            return msg, mpls
 
     def serialize(self, payload, prev):
-        return struct.pack(ethernet._PACK_STR, self.dst, self.src,
-                           self.ethertype)
-
-
-# copy vlan _TYPES
-ethernet._TYPES = vlan.vlan._TYPES
-ethernet.register_packet_type(vlan.vlan, ether.ETH_TYPE_8021Q)
-ethernet.register_packet_type(mpls.mpls, ether.ETH_TYPE_MPLS)
+        val = self.label << 12 | self.exp << 9 | self.bsb << 8 | self.ttl
+        return struct.pack(mpls._PACK_STR, val)
