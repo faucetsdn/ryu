74d90c7abaf31d2a037063f63cba1f6ce29498d0
HIYAMA Manabu <hiyama.manabu@po.ntts.co.jp>
2013-02-06 11:17:30 +0900

    packet lib: fix a bug in ipv6
    ERROR: Failure: AttributeError ('module' object has no attribute 'IPPROTO_ICMP6')
    ----------------------------------------------------------------------
    Traceback (most recent call last):
    (...)
      File "/home/openflow/git/osrg/ryu/ryu/lib/packet/ipv6.py", line 68, in <module>
        ipv6.register_packet_type(icmp6.icmp6, inet.IPPROTO_ICMP6)
    AttributeError: 'module' object has no attribute 'IPPROTO_ICMP6'
    
    Signed-off-by: HIYAMA Manabu <hiyama.manabu@po.ntts.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/lib/packet/ipv6.py
+++ ryu/lib/packet/ipv6.py
@@ -65,5 +65,5 @@ class ipv6(packet_base.PacketBase):
                          self.src, self.dst)
         return hdr
 
-ipv6.register_packet_type(icmp6.icmp6, inet.IPPROTO_ICMP6)
+ipv6.register_packet_type(icmp6.icmp6, inet.IPPROTO_ICMPV6)
 ipv6.register_packet_type(tcp.tcp, inet.IPPROTO_TCP)
