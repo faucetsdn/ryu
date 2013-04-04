e728b9c39d3e858d74b09367b9be3fa983a17ada
Isaku Yamahata <yamahata@valinux.co.jp>
2013-04-04 15:40:37 +0900

    debug_console: monkey patch code.InterractiveConsole.raw_input
    By default code.InteractiveConsole.raw_input uses builtin raw_input
    which is written in C. So it may not yeild thread execution, and other
    threads aren't scheduled.
    replace code.InteractiveConsole.raw_input with thread friendly one.
    Thus Ryu works as expected with debug_console app.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/app/debug_console.py
+++ ryu/app/debug_console.py
@@ -17,21 +17,29 @@
 # a RyuApp to provide a python interactive console for debugging
 
 import code
-import gevent
 import os
+import select
 import sys
 import signal
 
 from ryu.base import app_manager
+from ryu.lib import hub
+
+
+# builtin raw_input() is written by C and doesn't yeild execution.
+def _raw_input(message):
+    sys.stdout.write(message)
+    select.select([sys.stdin.fileno()], [], [])
+    return sys.stdin.readline()
 
 
 class DebugConsole(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(DebugConsole, self).__init__(*args, **kwargs)
-        gevent.spawn(self.__thread)
+        hub.spawn(self.__thread)
 
     def __thread(self):
-        code.InteractiveConsole().interact("Ryu Debug Console")
+        code.interact(banner="Ryu Debug Console", readfunc=_raw_input)
 
         # XXX should be a graceful shutdown
         os.kill(os.getpid(), signal.SIGTERM)
