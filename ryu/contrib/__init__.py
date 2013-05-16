63b652f78648a6afea212d39c3bacf915d29b394
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-05-16 12:33:55 +0900

    add a method to import a system (non ryu/contrib) module
    will be used for rpc library.

--- ryu/contrib/__init__.py
+++ ryu/contrib/__init__.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2013 Nippon Telegraph and Telephone Corporation.
+# Copyright (C) 2013 YAMAMOTO Takashi <yamamoto at valinux co jp>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Adjust module loading path for third party libraries
+
 import os
 import sys
+
+_original_sys_path = list(sys.path)
 
 for path in __path__:
     if path in sys.path:
@@ -9,3 +29,14 @@ for path in __path__:
     if path in sys.path:
         sys.path.remove(path)
     sys.path.insert(0, path)  # prioritize our own copy than system's
+
+def import_system(module):
+    """import a system module
+    """
+    saved_path = sys.path
+    try:
+        sys.path = _original_sys_path
+        __import__(module)
+    finally:
+        sys.path = saved_path
+    return sys.modules[module]
