f09407e4d6660a34a35aca12fb28d7e0a942eb72
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2013-05-18 06:00:50 +0900

    docs: fix ryu module import
    readthedocs.org complains about the failure about ryu module import.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,12 +12,13 @@
 # serve to show the default.
 
 import sys, os
-from ryu import version as ryu_version
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+
+from ryu import version as ryu_version
 
 # -- General configuration -----------------------------------------------------
 
