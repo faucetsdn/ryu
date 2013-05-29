cc657f94afba11cfc8857c93955f65816b82165a
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2013-05-29 17:32:31 +0900

    modified rpc_client.py to be able to specify parameters like vrid, priority as command args

--- ryu/tests/vrrp/rpc_client.py
+++ ryu/tests/vrrp/rpc_client.py
@@ -17,38 +17,44 @@ ex. sudo PYTHONPATH=. ./bin/ryu-manager \
 
 2. client side
 
-PYTHONPATH=. python ./ryu/tests/vrrp/rpc_client.py <server-host> <server-port> <method>
-
-ex.
-  PYTHONPATH=. python ./ryu/tests/vrrp/rpc_client.py 127.0.0.1 51720 config
+PYTHONPATH=. python ./ryu/tests/vrrp/rpc_client.py --host <server-host> --port <server-port> \
+                    --method <method> --vrid <vrid> --priority <priority> --ifname <ifname> --ifipaddr <ipaddr>
 
   method: config
           config_change
           list
+
+ex.
+  PYTHONPATH=. python ./ryu/tests/vrrp/rpc_client.py 127.0.0.1 51720 config
+  PYTHONPATH=. python ./ryu/tests/vrrp/rpc_client.py --host 172.16.10.1 --port 51820 --method config \
+                    --vrid 10 --priority 255 --ifname veth1 --ifipaddr 172.16.10.1
+
 
   Client wait notification from the server if you specify config method.
 
 
 """
 
+import socket
+import sys
+
 from ryu.lib import rpc
 from ryu.services.vrrp import rpc_manager
 from ryu.lib.packet import vrrp
 from ryu.lib import hub
 
-import socket
-import sys
+from argparse import ArgumentParser
 
 hub.patch()
 
 
-def vrrp_config(client):
-    vrid = 1
-    virtual_ipaddr = "10.0.0.1"
-    ifname = "veth0"
-    nic_ipaddr = "10.0.0.101"
-    priority = 150
-    vlan_id = 100
+def vrrp_config(client, args):
+    vrid = args.vrid
+    virtual_ipaddr = args.vripaddr
+    ifname = args.ifname
+    nic_ipaddr = args.ifipaddr
+    priority = args.priority
+    vlan_id = args.ifvlanid
 
     vrrp_param = rpc_manager.VRRPParam(vrrp.VRRP_VERSION_V3, vrid, virtual_ipaddr)
     vrrp_param.setPort(ifname, nic_ipaddr, priority, vlan_id)
@@ -56,15 +62,16 @@ def vrrp_config(client):
     print "api result : ", result
 
 
-def vrrp_config_change(client):
-    change_param = {rpc_manager.CONF_KEY_PRIORITY: 199, rpc_manager.CONF_KEY_ADVERTISEMENT_INTERVAL: 3}
-    vrid = 1
+def vrrp_config_change(client, args):
+    change_param = {rpc_manager.CONF_KEY_PRIORITY: args.priority,
+                    rpc_manager.CONF_KEY_ADVERTISEMENT_INTERVAL: args.interval}
+    vrid = args.vrid
     result = client.call("vrrp_config_change", [vrid, change_param])
     print "api result : ", result
 
 
-def vrrp_list(client):
-    vrid = 1
+def vrrp_list(client, args):
+    vrid = args.vrid
     result = client.call("vrrp_list", [vrid])
     info = result[0]
     for key in info:
@@ -86,29 +93,41 @@ def notification_callback(msg):
     print msg
 
 
-def main(host, port, method):
+def main(args):
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.connect((host, port))
+    client_sock.connect((args.host, args.port))
     client = rpc.Client(client_sock, notification_callback=notification_callback)
 
+    method = args.method
     print "target method : ", method
     if method == "config":
-        vrrp_config(client)
+        vrrp_config(client, args)
         hub.spawn(receive_notification_loop, client)
         while True:
             hub.sleep(0.5)
 
     elif method == "list":
-        vrrp_list(client)
+        vrrp_list(client, args)
 
     elif method == "config_change":
-        vrrp_config_change(client)
+        vrrp_config_change(client, args)
     else:
         print "target method is nothing."
 
+def _parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--method", dest="method", type=str, default="config")
+    parser.add_argument("--vrid", dest="vrid", type=int, default=16)
+    parser.add_argument("--vripaddr", dest="vripaddr", type=str, default="172.16.10.10")
+    parser.add_argument("--ifname", dest="ifname", type=str, default="veth1")
+    parser.add_argument("--ifipaddr", dest="ifipaddr", type=str, default="172.16.10.1")
+    parser.add_argument("--ifvlanid", dest="ifvlanid", type=int, default=None)
+    parser.add_argument("--priority", dest="priority", type=int, default="100")
+    parser.add_argument("--interval", dest="interval", type=int, default="1")
+    parser.add_argument("--host", dest="host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", dest="port", type=int, default="51820")
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    method = sys.argv[3]
-    main(host, port, method)
+    main(_parse_args())
