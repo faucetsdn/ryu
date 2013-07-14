fdcee50aa5c6be603a1c7b99e69f38ab9023fc38
Sam Russell <sam.h.russell@gmail.com>
2013-07-14 20:55:04 +1200

    attributions

--- ryu/app/simple_switch_12.py
+++ ryu/app/simple_switch_12.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+# OpenFlow 1.2 upgrade by Sam Russell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
