31eef5bf183f3f594b3a46d0e6233c7d72ac3237
Sam Russell <sam.h.russell@gmail.com>
2013-07-15 20:18:26 +1200

    import dpset

--- ryu/app/spe.py
+++ ryu/app/spe.py
@@ -20,6 +20,7 @@ import struct
 from ryu.base import app_manager
 from ryu.controller import mac_to_port
 from ryu.controller import ofp_event
+from ryu.controller import dpset
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_2, ofproto_v1_2_parser
