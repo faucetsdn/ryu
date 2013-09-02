673c811f85f57214b8be0cf69d6252ccc404bc11
Yoshihiro Kaneko <ykaneko0929@gmail.com>
2013-09-02 19:21:20 +0900

    doc: update reference page for ofproto.
    Signed-off-by: Yoshihiro Kaneko <ykaneko0929@gmail.com>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ from ryu import version as ryu_version
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'sphinx.ext.autodoc' ]
+extensions = [ 'sphinx.ext.autodoc', 'sphinx.ext.autosummary' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
