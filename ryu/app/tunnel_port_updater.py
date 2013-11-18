d56644eb6a03ddd73a6906dd712e4ce66ba0c4f5
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-11-18 17:18:17 +0900

    ryu.app.tunnel_port_updater: comments
    Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/app/tunnel_port_updater.py
+++ ryu/app/tunnel_port_updater.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This module updates OVS tunnel ports for OpenStack integration.
+
 import collections
 from oslo.config import cfg
 import logging
