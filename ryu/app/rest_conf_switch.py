24e3953d75d70a82f4b1de21a23aac8edc3affb1
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-11-18 17:18:14 +0900

    ryu.app.rest_conf_switch: comments
    Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/app/rest_conf_switch.py
+++ ryu/app/rest_conf_switch.py
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This module provides a set of REST API for switch configuration.
+#   - Per-switch Key-Value store
+#
+# Used by OpenStack Ryu agent.
+
 import httplib
 import json
 import logging
