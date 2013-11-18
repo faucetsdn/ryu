d04f89fa44981c36b7e6353579473504043e9d56
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2013-11-18 17:18:13 +0900

    ryu.app.rest: comments
    Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/app/rest.py
+++ ryu/app/rest.py
@@ -14,6 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This module provides a basic set of REST API.
+#   - Network registration
+#   - End-point port management
+#       - OpenFlow port number
+#       - MAC address (for anti-spoofing)
+#
+# Used by OpenStack Ryu plug-in.
+
 import json
 from webob import Response
 
