2235def21b1b4b1e7662cd937a0e8bbee0162094
Simon Horman <horms@verge.net.au>
2014-01-29 12:05:59 +0900

    Add OF1.4 PushPbb action support
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/ofproto/ofproto_v1_4_parser.py
+++ ryu/ofproto/ofproto_v1_4_parser.py
@@ -2485,6 +2485,35 @@ class OFPActionSetField(OFPAction):
         yield (self.key, self.value)
 
 
+@OFPAction.register_action_type(ofproto.OFPAT_PUSH_PBB,
+                                ofproto.OFP_ACTION_PUSH_SIZE)
+class OFPActionPushPbb(OFPAction):
+    """
+    Push PBB action
+
+    This action pushes a new PBB header to the packet.
+
+    ================ ======================================================
+    Attribute        Description
+    ================ ======================================================
+    ethertype        Ether type
+    ================ ======================================================
+    """
+    def __init__(self, ethertype, type_=None, len_=None):
+        super(OFPActionPushPbb, self).__init__()
+        self.ethertype = ethertype
+
+    @classmethod
+    def parser(cls, buf, offset):
+        (type_, len_, ethertype) = struct.unpack_from(
+            ofproto.OFP_ACTION_PUSH_PACK_STR, buf, offset)
+        return cls(ethertype)
+
+    def serialize(self, buf, offset):
+        msg_pack_into(ofproto.OFP_ACTION_PUSH_PACK_STR, buf, offset,
+                      self.type, self.len, self.ethertype)
+
+
 @OFPAction.register_action_type(ofproto.OFPAT_POP_PBB,
                                 ofproto.OFP_ACTION_HEADER_SIZE)
 class OFPActionPopPbb(OFPAction):
