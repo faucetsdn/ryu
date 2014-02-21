7de6dbdb449f5728e30e310a4d66d005125660b8
Isaku Yamahata <yamahata@valinux.co.jp>
2014-02-21 17:14:03 +0900

    topology: split out exported API from the implementation
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

---
+++ ryu/topology/api.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2013 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.topology import event
+
+
+def get_switch(app, dpid=None):
+    rep = app.send_request(event.EventSwitchRequest(dpid))
+    return rep.switches
+
+
+def get_all_switch(app):
+    return get_switch(app)
+
+
+def get_link(app, dpid=None):
+    rep = app.send_request(event.EventLinkRequest(dpid))
+    return rep.links
+
+
+def get_all_link(app):
+    return get_link(app)
