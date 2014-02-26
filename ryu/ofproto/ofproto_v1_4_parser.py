7d0cff70445820b1bc0a588c903e818610c1e6cf
Simon Horman <horms@verge.net.au>
2014-02-26 16:26:00 +0900

    of14: Add port desc optical property
    This may be used in port stats and port status messages
    
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/ofproto/ofproto_v1_4_parser.py
+++ ryu/ofproto/ofproto_v1_4_parser.py
@@ -868,6 +868,37 @@ class OFPPortDescPropEthernet(StringifyMixin):
         return ether
 
 
+@OFPPortProp.register_type(ofproto.OFPPDPT_OPTICAL)
+class OFPPortDescPropOptical(StringifyMixin):
+    def __init__(self, type_=None, length=None, supported=None,
+                 tx_min_freq_lmda=None, tx_max_freq_lmda=None,
+                 tx_grid_freq_lmda=None, rx_min_freq_lmda=None,
+                 rx_max_freq_lmda=None, rx_grid_freq_lmda=None,
+                 tx_pwr_min=None, tx_pwr_max=None):
+        self.type = type_
+        self.length = length
+        self.supported = supported
+        self.tx_min_freq_lmda = tx_min_freq_lmda
+        self.tx_max_freq_lmda = tx_max_freq_lmda
+        self.tx_grid_freq_lmda = tx_grid_freq_lmda
+        self.rx_min_freq_lmda = rx_min_freq_lmda
+        self.rx_max_freq_lmda = rx_max_freq_lmda
+        self.rx_grid_freq_lmda = rx_grid_freq_lmda
+        self.tx_pwr_min = tx_pwr_min
+        self.tx_pwr_max = tx_pwr_max
+
+    @classmethod
+    def parser(cls, buf):
+        optical = cls()
+        (optical.type, optical.length, optical.supported,
+         optical.tx_min_freq_lmda, optical.tx_max_freq_lmda,
+         optical.tx_grid_freq_lmda, optical.rx_min_freq_lmda,
+         optical.rx_max_freq_lmda, optical.rx_grid_freq_lmda,
+         optical.tx_pwr_min, optical.tx_pwr_max) = struct.unpack_from(
+            ofproto.OFP_PORT_DESC_PROP_OPTICAL_PACK_STR, buf, 0)
+        return optical
+
+
 class OFPTableModProp(OFPPropBase):
     _TYPES = {}
 
