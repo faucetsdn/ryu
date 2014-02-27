55b89f0bd8f41f443b3f4d924ebf25a2482092ab
Simon Horman <horms@verge.net.au>
2014-02-27 17:56:15 +0900

    of14: Add port mod optical property
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- ryu/ofproto/ofproto_v1_4_parser.py
+++ ryu/ofproto/ofproto_v1_4_parser.py
@@ -5121,6 +5121,30 @@ class OFPPortModPropEthernet(StringifyMixin):
         return buf
 
 
+class OFPPortModPropOptical(StringifyMixin):
+    def __init__(self, type_=None, length=None, configure=None,
+                 freq_lmda=None, fl_offset=None, grid_span=None,
+                 tx_pwr=None):
+        self.type = type_
+        self.length = length
+        self.configure = configure
+        self.freq_lmda = freq_lmda
+        self.fl_offset = fl_offset
+        self.grid_span = grid_span
+        self.tx_pwr = tx_pwr
+
+    def serialize(self):
+        # fixup
+        self.length = struct.calcsize(
+            ofproto.OFP_PORT_MOD_PROP_OPTICAL_PACK_STR)
+
+        buf = bytearray()
+        msg_pack_into(ofproto.OFP_PORT_MOD_PROP_OPTICAL_PACK_STR, buf, 0,
+                      self.type, self.length, self.configure, self.freq_lmda,
+                      self.fl_offset, self.grid_span, self.tx_pwr)
+        return buf
+
+
 @_set_msg_type(ofproto.OFPT_PORT_MOD)
 class OFPPortMod(MsgBase):
     """
