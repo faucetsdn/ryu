faucetsdn/ryu

commit 5a4d429a6d66b587a8dcee2d7749941df05610b1

Add band name to meter stats

ryu/controller/api.py
@@ -278,6 +278,13 @@ class RpcOFPManager(app_manager.RyuApp):
                 self.stats_log.info(json.dumps(stats))
                 self.logger.info(_(msg=stats, log_type='stats'))
 
+    def _add_band_name(self, stats):
+       new_stats = [
+           {'band_name':{'pir':stats[0]}},
+           {'band_name':{'cir':stats[1]}}
+       ]
+       return new_stats
+
     @handler.set_ev_cls(ofp_event.EventOFPMeterStatsReply,
                         handler.MAIN_DISPATCHER)
     def _meter_stats_reply_handler(self, ev):
@@ -287,6 +294,7 @@ class RpcOFPManager(app_manager.RyuApp):
             if stat.meter_id in self.monitored_meters:
                 contexts = self.monitored_meters[stat.meter_id]
                 stats = stat.to_jsondict()['OFPMeterStats']
+                stats['band_stats'] = self._add_band_name(stats['band_stats'])
                 stats.update(contexts)
                 self.stats_log.info(json.dumps(stats))
                 self.logger.info(_(msg=stats, log_type='stats'))

commit e94ee078eee069a732204368803e5de895d82ba6

apgw: fix unittest failure
fix unittest failure caused by 5a4d429a6d66b587a8dcee2d7749941df05610b1.

also fix pep8

Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

ryu/controller/api.py
@@ -275,11 +275,11 @@ class RpcOFPManager(app_manager.RyuApp):
                 self.logger.info(_(msg=stats, log_type='stats'))
 
     def _add_band_name(self, stats):
-       new_stats = [
-           {'band_name':{'pir':stats[0]}},
-           {'band_name':{'cir':stats[1]}}
-       ]
-       return new_stats
+        new_stats = [
+            {'band_name': {'pir': stats[0]}},
+            {'band_name': {'cir': stats[1]}}
+            ]
+        return new_stats
 
     @handler.set_ev_cls(ofp_event.EventOFPMeterStatsReply,
                         handler.MAIN_DISPATCHER)

ryu/tests/unit/app/test_apgw_rpc.py
@@ -739,7 +739,8 @@ class TestRpcOFPManager(unittest.TestCase):
         s = ofpp.OFPMeterStats(meter_id=meter_id, flow_count=10,
                                packet_in_count=10, byte_in_count=10,
                                duration_sec=10, duration_nsec=10,
-                               band_stats=[])
+                               band_stats=[ofpp.OFPMeterBandStats(1, 8),
+                                           ofpp.OFPMeterBandStats(2, 16)])
 
         msg.body = [s]
         m._meter_stats_reply_handler(ev)