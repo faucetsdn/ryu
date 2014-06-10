faucetsdn/ryu

commit d3f8f4eb4cfa3a89164e4ef723d1cc80eb07a68c

RyuApp: delete a key without handlers when unregistering a handler
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

ryu/base/app_manager.py
@@ -169,6 +169,8 @@ class RyuApp(object):
     def unregister_handler(self, ev_cls, handler):
         assert callable(handler)
         self.event_handlers[ev_cls].remove(handler)
+        if not event_handlers[ev_cls]:
+            del event_handlers[ev_cls]
 
     def register_observer(self, ev_cls, name, states=None):
         states = states or set()

commit ed25eefa846e2f8b7a6683908f224efc2bc0acc9

app_manager: Fix a crash in unregister_handler
This fixes a regression introduced by commit d3f8f4eb.
("RyuApp: delete a key without handlers when unregistering a handler")

Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

ryu/base/app_manager.py
@@ -178,8 +178,8 @@ class RyuApp(object):
     def unregister_handler(self, ev_cls, handler):
         assert callable(handler)
         self.event_handlers[ev_cls].remove(handler)
-        if not event_handlers[ev_cls]:
-            del event_handlers[ev_cls]
+        if not self.event_handlers[ev_cls]:
+            del self.event_handlers[ev_cls]
 
     def register_observer(self, ev_cls, name, states=None):
         states = states or set()