6ef43b6f7d37d15cd4d0e7a3a8d5f8423cfc9d5d
muzixing <350959853@qq.com>
2014-10-24 12:01:52 +0800

    add_arp_proxy_13
    arp_proxy_13 is a ryu app:
    * Reply ARP requests
    * Avoid broadcast storm instead of STP

--- ryu/app/arp_proxy_13.py
+++ ryu/app/arp_proxy_13.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author:Li Cheng 
+# Contributor:Li Cheng @BUPT
 # Homepage:www.muzixing.com
 # Time:2014/10/19
 #
