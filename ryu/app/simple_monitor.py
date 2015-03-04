f648d2504786e2bf85b63e62e04d0137bca5f0ed
muzixing <350959853@qq.com>
2015-03-04 11:32:25 +0800

    add simple_monitor.py

--- ryu/app/simple_monitor.py
+++ ryu/app/simple_monitor.py
@@ -1,14 +1,16 @@
 from __future__ import division
 from operator import attrgetter
-
+from ryu.base import app_manager
 from ryu.app import simple_switch_13
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
+from ryu.lib.packet import packet
 
 
-class SimpleMonitor(simple_switch_13.SimpleSwitch13):
+class SimpleMonitor(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(SimpleMonitor, self).__init__(*args, **kwargs)
@@ -19,6 +21,7 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
         self.flow_stats = {}
         self.flow_speed = {}
         self.sleep = 2
+        self.state_len = 3
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -32,6 +35,15 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
             if datapath.id in self.datapaths:
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
+
+    # get the ports' features.
+    @set_ev_cls(
+        ofp_event.EventOFPStateChange,
+        [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def port_features_handler(self, ev):
+        datapath = ev.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
     def _monitor(self):
         while True:
@@ -71,21 +83,9 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
 
-        # self.logger.info('datapath         '
-        #                  'in-port  eth-dst           '
-        #                  'out-port packets  bytes')
-        # self.logger.info('---------------- '
-        #                  '-------- ----------------- '
-        #                  '-------- -------- --------')
-
         for stat in sorted([flow for flow in body if flow.priority == 1],
                            key=lambda flow: (flow.match['in_port'],
                                              flow.match['eth_dst'])):
-            # self.logger.info('%016x %8x %17s %8x %8d %8d',
-            #                  ev.msg.datapath.id,
-            #                  stat.match['in_port'], stat.match['eth_dst'],
-            #                  stat.instructions[0].actions[0].port,
-            #                  stat.packet_count, stat.byte_count)
 
             key = (
                 stat.match['in_port'], stat.match['eth_dst'],
@@ -93,7 +93,7 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
             value = (
                 stat.packet_count, stat.byte_count,
                 stat.duration_sec, stat.duration_nsec)
-            self._save_stats(self.flow_stats, key, value, 5)
+            self._save_stats(self.flow_stats, key, value, self.state_len)
 
             # Get flow's speed.
             pre = 0
@@ -104,36 +104,23 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
                 period = self._get_period(
                     tmp[-1][2], tmp[-1][3],
                     tmp[-2][2], tmp[-2][3])
-                # print "flow period: ", period
 
             speed = self._get_speed(
                 self.flow_stats[key][-1][1], pre, period)
 
-            self._save_stats(self.flow_speed, key, speed, 5)
-            print '\n Flow Speed:\n', self.flow_speed
+            self._save_stats(self.flow_speed, key, speed, self.state_len)
 
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def _port_stats_reply_handler(self, ev):
         body = ev.msg.body
 
-        # self.logger.info('datapath         port     '
-        #                 'rx-pkts  rx-bytes rx-error '
-        #                 'tx-pkts  tx-bytes tx-error')
-        # self.logger.info('---------------- -------- '
-        #                 '-------- -------- -------- '
-        #                 '-------- -------- --------')
         for stat in sorted(body, key=attrgetter('port_no')):
-            # self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-            #                  ev.msg.datapath.id, stat.port_no,
-            #                  stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-            #                  stat.tx_packets, stat.tx_bytes, stat.tx_errors)
-
             key = (ev.msg.datapath.id, stat.port_no)
             value = (
                 stat.rx_packets, stat.rx_bytes, stat.rx_errors,
                 stat.duration_sec, stat.duration_nsec)
 
-            self._save_stats(self.port_stats, key, value, 5)
+            self._save_stats(self.port_stats, key, value, self.state_len)
 
             # Get port speed.
             pre = 0
@@ -148,5 +135,5 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
             speed = self._get_speed(
                 self.port_stats[key][-1][1], pre, period)
 
-            self._save_stats(self.port_speed, key, speed, 5)
+            self._save_stats(self.port_speed, key, speed, self.state_len)
             print '\n Speed:\n', self.port_speed
