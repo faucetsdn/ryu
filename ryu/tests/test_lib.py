e14c49abc891f132f867943ca5f5c7eae7a7c763
YAMAMOTO Takashi <yamamoto@valinux.co.jp>
2015-05-15 11:03:31 +0900

    test_lib: Remove a questional exception re-raising
    This "raise" makes the following code to fall back to non-color
    unreachable.  While this has been so since the initial version,
    recently it started biting us on travis-ci.
    (I haven't investigated why.  It might or might not be related to
    recently released tox 2.0's "ENV isolation".)
    
    Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- ryu/tests/test_lib.py
+++ ryu/tests/test_lib.py
@@ -58,7 +58,6 @@ class _AnsiColorizer(object):
                     curses.setupterm()
                     return curses.tigetnum("colors") > 2
             except:
-                raise
                 # guess false in case of error
                 return False
     supported = classmethod(supported)
