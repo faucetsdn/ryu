903e7872c2d2f47707ea3f80072f301655f1402a
None <None>
2015-07-08 13:12:26 +0800

    add files of network aware

---
+++ ryu/app/network_aware/network_aware.py
@@ -0,0 +1,211 @@
+# conding=utf-8
+import logging
+import struct
+from operator import attrgetter
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import arp
+from ryu.lib import hub
+
+from ryu.topology import event, switches
+from ryu.topology.api import get_switch, get_link
+
+SLEEP_PERIOD = 10
+IS_UPDATE = True
+
+
+class Network_Aware(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _NAME = 'network_aware'
+
+    def __init__(self, *args, **kwargs):
+        super(Network_Aware, self).__init__(*args, **kwargs)
+        self.name = "Network_Aware"
+        self.topology_api_app = self
+        self.link_list = []
+
+        # links   :(src_dpid,dst_dpid)->(src_port,dst_port)
+        self.link_to_port = {}
+
+        # {(sw,port) :[host1_ip,host2_ip,host3_ip,host4_ip]}
+        self.access_table = {}
+
+        # ports
+        self.switch_port_table = {}  # dpid->port_num
+
+        # dpid->port_num (ports without link)
+        self.access_ports = {}
+
+        # dpid->port_num(ports with contain link `s port)
+        self.interior_ports = {}
+        self.graph = {}
+
+        self.pre_link_to_port = {}
+        self.pre_graph = {}
+        self.pre_access_table = {}
+
+        self.monitor_thread = hub.spawn(self._monitor)
+
+    # show topo ,and get topo again
+    def _monitor(self):
+        i = 0
+        while True:
+            self.show_topology()
+            if i == 5:
+                self.get_topology(None)
+                i = 0
+            hub.sleep(SLEEP_PERIOD)
+            i = i + 1
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        msg = ev.msg
+        self.logger.info("switch:%s connected", datapath.id)
+
+        # install table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, dp, p, match, actions, idle_timeout=0, hard_timeout=0):
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=dp, priority=p,
+                                idle_timeout=idle_timeout,
+                                hard_timeout=hard_timeout,
+                                match=match, instructions=inst)
+        dp.send_msg(mod)
+
+    def get_switches(self):
+        return self.switches
+
+    def get_links(self):
+        return self.link_to_port
+
+    # get Adjacency matrix from link_to_port
+    def get_graph(self, link_list):
+        for src in self.switches:
+            for dst in self.switches:
+                self.graph.setdefault(src, {dst: float('inf')})
+                if src == dst:
+                    self.graph[src][src] = 0
+                elif (src, dst) in link_list:
+                    self.graph[src][dst] = 1
+                else:
+                    self.graph[src][dst] = float('inf')
+        return self.graph
+
+        for sw in switch_list:
+            dpid = sw.dp.id
+            self.switch_port_table.setdefault(dpid, set())
+            self.interior_ports.setdefault(dpid, set())
+            self.access_ports.setdefault(dpid, set())
+
+            for p in sw.ports:
+                self.switch_port_table[dpid].add(p.port_no)
+
+    def create_port_map(self, switch_list):
+        for sw in switch_list:
+            dpid = sw.dp.id
+            self.switch_port_table.setdefault(dpid, set())
+            self.interior_ports.setdefault(dpid, set())
+            self.access_ports.setdefault(dpid, set())
+
+            for p in sw.ports:
+                self.switch_port_table[dpid].add(p.port_no)
+
+    # get links`srouce port to dst port  from link_list,
+    # link_to_port:(src_dpid,dst_dpid)->(src_port,dst_port)
+    def create_interior_links(self, link_list):
+        for link in link_list:
+            src = link.src
+            dst = link.dst
+            self.link_to_port[
+                (src.dpid, dst.dpid)] = (src.port_no, dst.port_no)
+
+            # find the access ports and interiorior ports
+            if link.src.dpid in self.switches:
+                self.interior_ports[link.src.dpid].add(link.src.port_no)
+            if link.dst.dpid in self.switches:
+                self.interior_ports[link.dst.dpid].add(link.dst.port_no)
+
+    # get ports without link into access_ports
+    def create_access_ports(self):
+        for sw in self.switch_port_table:
+            self.access_ports[sw] = self.switch_port_table[
+                sw] - self.interior_ports[sw]
+
+    events = [event.EventSwitchEnter,
+              event.EventSwitchLeave, event.EventPortAdd,
+              event.EventPortDelete, event.EventPortModify,
+              event.EventLinkAdd, event.EventLinkDelete]
+
+    @set_ev_cls(events)
+    def get_topology(self, ev):
+        switch_list = get_switch(self.topology_api_app, None)
+        self.create_port_map(switch_list)
+        self.switches = self.switch_port_table.keys()
+        links = get_link(self.topology_api_app, None)
+        self.create_interior_links(links)
+        self.create_access_ports()
+        self.get_graph(self.link_to_port.keys())
+        # self.show_topology()
+
+    # show topo
+    def show_topology(self):
+        switch_num = len(self.graph)
+        if self.pre_graph != self.graph or IS_UPDATE:
+            print "---------------------Topo Link---------------------"
+            print '%10s' % ("switch"),
+            for i in xrange(1, switch_num + 1):
+                print '%10d' % i,
+            print ""
+            for i in self.graph.keys():
+                print '%10d' % i,
+                for j in self.graph[i].values():
+                    print '%10.0f' % j,
+                print ""
+            self.pre_graph = self.graph
+        # show link
+        if self.pre_link_to_port != self.link_to_port or IS_UPDATE:
+            print "---------------------Link Port---------------------"
+            print '%10s' % ("switch"),
+            for i in xrange(1, switch_num + 1):
+                print '%10d' % i,
+            print ""
+            for i in xrange(1, switch_num + 1):
+                print '%10d' % i,
+                for j in xrange(1, switch_num + 1):
+                    if (i, j) in self.link_to_port.keys():
+                        print '%10s' % str(self.link_to_port[(i, j)]),
+                    else:
+                        print '%10s' % "No-link",
+                print ""
+            self.pre_link_to_port = self.link_to_port
+
+        # each dp access host
+        # {(sw,port) :[host1_ip,host2_ip,host3_ip,host4_ip]}
+        if self.pre_access_table != self.access_table or IS_UPDATE:
+            print "----------------Access Host-------------------"
+            print '%10s' % ("switch"), '%12s' % "Host"
+            if not self.access_table.keys():
+                print "    NO found host"
+            else:
+                for tup in self.access_table:
+                    print '%10d:    ' % tup[0], self.access_table[tup]
+            self.pre_access_table = self.access_table
---
+++ ryu/app/network_aware/tree.py
@@ -0,0 +1,69 @@
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import Link, Intf, TCLink
+from mininet.topo import Topo
+import logging
+import os
+
+class CustomTopo(Topo):
+    "Simple Data Center Topology"
+
+    "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
+    "fanout - number of child switch per parent switch"
+    def __init__(self,**opts):
+	Topo.__init__(self, **opts)
+	s=[]
+	   
+	s.append(self.addSwitch('s1'))
+	s.append(self.addSwitch('s2'))
+	s.append(self.addSwitch('s3'))
+	s.append(self.addSwitch('s4'))
+        s.append(self.addSwitch('s5'))
+        s.append(self.addSwitch('s6'))    
+	self.addLink(s[0],s[1],bw=10)
+	self.addLink(s[1],s[2],bw=10)
+        self.addLink(s[2],s[0],bw=10)
+        self.addLink(s[0],s[3],bw=5)
+        self.addLink(s[1],s[4],bw=5)
+        self.addLink(s[2],s[5],bw=5)
+
+	h1=self.addHost('h1',mac='00:00:00:00:00:01')
+	h2=self.addHost('h2',mac='00:00:00:00:00:02')
+	h3=self.addHost('h3',mac='00:00:00:00:00:03')
+	h4=self.addHost('h4',mac='00:00:00:00:00:04')
+	h5=self.addHost('h5',mac='00:00:00:00:00:05')
+        h6=self.addHost('h6',mac='00:00:00:00:00:06')
+    
+        self.addLink(s[3],h1,bw=1)
+	self.addLink(s[3],h2,bw=1)
+   	self.addLink(s[4],h3,bw=1)	
+	self.addLink(s[4],h4,bw=1)
+        self.addLink(s[5],h5,bw=1)
+        self.addLink(s[5],h6,bw=1)
+
+topos = {'custom': (lambda: CustomTopo())}
+
+def createTopo():
+    logging.debug("Create Topo")
+    topo = CustomTopo()
+
+    logging.debug("Start Mininet")
+    CONTROLLER_IP = "127.0.0.1"
+    CONTROLLER_PORT = 6633
+    net = Mininet(topo=topo, link=TCLink, controller=None)
+    net.addController(
+        'controller', controller=RemoteController,
+        ip=CONTROLLER_IP, port=CONTROLLER_PORT)
+
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    if os.getuid() != 0:
+        logger.debug("You are NOT root")
+    elif os.getuid() == 0:
+        createTopo()
