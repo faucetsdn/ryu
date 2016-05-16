537321ae5b0e5fc38ab1d0d966bba9a13102fcd5
muzixing <350959853@qq.com>
2016-05-16 15:53:18 +0800

    delete useless files.

--- ryu/app/multipath.py
+++ ryu/app/multipath.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+# Copyright (C) 2016 Li Cheng BUPT www.muzixing.com.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
